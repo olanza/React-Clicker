a47b2cecad009866ee918f6c8493fe64d1e75938
olanza <olanza95@gmail.com>
2019-06-25 23:24:36 -0400

    Updates

---
+++ precache-manifest.5215720ede15a9a394b394e34e010b63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a2fc91f086323943a04d961d8cc998",
+    "url": "/React-Clicker/index.html"
+  },
+  {
+    "revision": "9d48c37b80e91c55d43d",
+    "url": "/React-Clicker/static/css/main.7143746e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/React-Clicker/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "9d48c37b80e91c55d43d",
+    "url": "/React-Clicker/static/js/main.05888aae.chunk.js"
+  },
+  {
+    "revision": "9be7498d8b7cb66183be",
+    "url": "/React-Clicker/static/js/runtime~main.ac24970d.js"
+  }
+]);
